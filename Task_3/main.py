ca5a68ffc1bbbfabf707e3eb74db97c75c9f0922
Artur Shamiyarov <dz3nls7@gmail.com>
2022-10-22 23:11:03 +0300

    fixed dbscan

--- Task_3/main.py
+++ Task_3/main.py
@@ -1,107 +1,82 @@
-import pygame
 import numpy as np
+import pygame
 import random
 
 
-def dist(pntA, pntB):
-    return np.sqrt((pntA[0] - pntB[0]) ** 2 + (pntA[1] - pntB[1]) ** 2)
+# todo: right version dbscan
+
+def near_by_points(points, idx, epsilon):
+    near = []
+    for point_idx in range(0, len(points)):
+        if np.linalg.norm(points[idx] - points[point_idx]) < epsilon:
+            near.append(point_idx)
+    return near
 
 
-def dbscan(points):
-    eps = 30
-    #  todo: точки шум
-    NOISE = []
-    print("dbscan -- points: ", points)
-    #  todo: найдем все core точки
-    all_core_points, not_tagged_points = set_flags(points)
-    #  todo: получаем индексы точек соответствующие условию по соседям all_core_points
-    #  todo: и список индексов не помеченных точек not_tagged_points
+def draw(list_col, clusters):
+    for point, cluster in zip(list_col, clusters):
+        if dic.get(cluster) == None:
+            color = "#" + ''.join([random.choice('0123456789ABCDEF') for i in range(6)])
+            print("color: ", color)
+            dic[cluster] = color
 
-    #   todo: находим все крайние точки и шум
-    none_core_points = []
-    for ntp_id, ntp in enumerate(not_tagged_points):
-        #  todo: если хотя бы один сосед ntp2 зеленый то выдаем желтый флаг
-        flag = 0
-        for acp_id, acp in enumerate(all_core_points):
-            if dist(points[ntp], points[acp]) < eps and points[ntp] != points[acp]:
-                flag = 1
-                break
-            else:
-                flag = 0
-        if flag == 1:
-            none_core_points.append(ntp)
+            print(dic)
+        radius = 8
+        if cluster == -1:
+            pygame.draw.circle(screen, "Black", point, radius)
         else:
-            NOISE.append(ntp)
-    print("none_core_points_2: ", none_core_points)
-    print("NOISE2: ", NOISE)
-
-    return all_core_points, none_core_points, NOISE
+            pygame.draw.circle(screen, dic.get(cluster), point, radius)
 
 
-def set_flags(points):
-    # todo: eps - радиус вокруг точки в области которого могут находиться другие точки
-    eps = 30
-    # todo: минимальное кол-во точек в радиусе одной точки для образования кластера
-    minPts = 3
-    flags = []
-    others_points = []
-    for i, pnt in enumerate(points):
-        neighb = 0
-        # todo: для каждой точки из списка проходимся по точкам из того же списка: aa ab ac и тд
-        for p in points:
-            #  todo: если дистанция между точками a и b меньше радиусной и точка не равна сама себе
-            if dist(pnt, p) < eps and pnt != p:
-                #  todo: добавляем соседа для точки a
-                neighb += 1
-        #  todo: если кол-во соседних точек для этой точки больше или равно минимальному кол-ву соседей
-        if neighb >= minPts:
-            #  todo: добавляем индекс этой точки в список помеченных как core точки
-            flags.append(i)
-        else:
-            #  todo: если не подходит добавляем индекс этой точки в список остальных точек
-            others_points.append(i)
-    #  todo: возвращаем индексы точек соответствующие условию по соседям
-    return flags, others_points
+def algorithm(_points, _epsilon):
+    labels = [0] * len(_points)  # создаем пустой лист на основе количестива точек
+    cluster_idx = 0
+    minimum_points = 3
+    for i in range(0, len(_points)):
+        if not (labels[i] == 0):  # если точка не равна нулю пропускаем
+            continue
+        near_points = near_by_points(_points, i, _epsilon)  # если равна то находим ближайшие точки
+        if len(near_points) < minimum_points:  # если такая точка одна то присваиваем шум
+            labels[i] = -1
+        else:  # если таких точек несколько то создаем новый кластер
+            cluster_idx += 1
+            labels[i] = cluster_idx  # и присваеваем его к точке
+            i = 0
+            while i < len(near_points):  # проходи по близким точкам
+                point = near_points[i]
+                if labels[point] == -1:
+                    labels[point] = cluster_idx
+
+                elif labels[point] == 0:
+                    labels[point] = cluster_idx
+                    point_near = near_by_points(_points, point, _epsilon)
+                    if len(point_near) >= minimum_points:
+                        near_points = near_points + point_near
+                i += 1
+    return labels
 
 
-def draw():
-    print('draw')
-    # todo: радиус точки
-    R = 7
-    points = []
-    pygame.init()
-    # todo: настройка отображения
-    screen = pygame.display.set_mode([800, 600])
-    screen.fill(color='white')
-    while True:
-        for event in pygame.event.get():
-            # todo: функционал выхода
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-            # todo: -------------------------
-            # print(event)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                #  todo: если нажимаем лкм
-                if event.button == 1:
-                    #  todo: рисуем круги
-                    pygame.draw.circle(screen, 'black', event.pos, R)
-                    # todo: добавляем в список координаты нарисованной точки
-                    points.append(event.pos)
-                    # print(type(np.array(event.pos)))
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    print(1)
-                    id_clusters_points, n_core_points, noize_points = dbscan(points)
-                    for ic, idc in enumerate(id_clusters_points):
-                        pygame.draw.circle(screen, 'green', points[idc], R)
-                    for id_ncp, ncp_p in enumerate(n_core_points):
-                        pygame.draw.circle(screen, "yellow", points[ncp_p], R)
-                    for id_noize, noize_p in enumerate(noize_points):
-                        pygame.draw.circle(screen, "red", points[noize_p], R)
-            pygame.display.update()
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+screen.fill('WHITE')
+pygame.display.update()
+FPS = 60
+clock = pygame.time.Clock()
+dic = {}
+points = []
 
+epsilon = 40
 
-# todo: dbscan
-if __name__ == '__main__':
-    draw()
+play = True
+while play:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            play = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            points.append(pygame.mouse.get_pos())
+            screen.fill('WHITE')
+            prediction = algorithm(np.array(points), epsilon)
+            draw(points, prediction)
+
+    pygame.display.update()
